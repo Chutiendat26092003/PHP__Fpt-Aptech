597c3402def4882ecac5d8b880dcece95ba33a82
Chutiendat26092003 <chutiendat148@gmail.com>
2022-07-19 11:33:40 +0700

    MySQL

---
+++ buoi2/Bien-KieuDuLieu/vd.php
@@ -0,0 +1,34 @@
+<?php
+    //int
+    $int_var = 123456;
+    $another_int = -123456 + 123456;
+    echo "$int_var , $another_int <br /> ";
+    //double
+    $many = 2.2888800;
+    $many_2 = 2.2111200;
+    $few = $many + $many_2;
+    print ("$many + $many_2 = $few<br />");
+    //boolean
+    if (TRUE)
+        print ("This will always print<br />");
+    else
+        print ("This will never print<br />");
+
+    //null
+    $my_var = NULL; //or  $my_var = null;
+    //string
+
+    $string_1 = "This is a string in double quotes";
+    $string_2 = 'This is a somewhat longer, singly quoted string';
+    $string_39 = "This string has thirty-nine characters";
+    $string_0 = "";
+    $variable = "name";
+    $literally =  'My $variable will not print';
+    print ($literally);
+    print "<br>";
+    $literally = "My $variable will print";
+    print ($literally);
+
+
+
+?>
